f3c6f4fa8657d00df4a9d42ea7f7752f7c891efd
Ben Christel <bchristel@pivotal.io>
2022-05-13 18:34:07 -0700

    Dry tests with default tendencies
    Co-authored-by: Gavin Morgan <morgan.gavin@gmail.com>

--- src/notifications.js
+++ src/notifications.js
@@ -1,126 +1,93 @@
 // @flow
 import {test, expect, is} from "@benchristel/taste"
 
+const tendToSend = {
+    channelMuted: false,
+    threading: unthreaded(),
+    doNotDisturb: false,
+    doNotDisturbOverridden: true,
+    broadcast: true,
+    suppressBroadcast: false,
+    channelNotificationPreference: "everything",
+}
+
+const tendNotToSend = {
+    channelMuted: true,
+    threading: {type: "Threaded", subscribed: false},
+    doNotDisturb: true,
+    doNotDisturbOverridden: false,
+    broadcast: false,
+    suppressBroadcast: true,
+    channelNotificationPreference: "nothing",
+}
+
 test("notifications:", {
     "returns false if the channel is muted and the message is not in a thread"() {
         expect(shouldSend({
+            ...tendToSend,
             channelMuted: true,
             threading: unthreaded(),
-            doNotDisturb: false,
-            doNotDisturbOverridden: false,
-            broadcast: false,
-            suppressBroadcast: false,
-            channelNotificationPreference: "everything",
         }), is, false)
     },
 
     "sends a notification"() {
-        expect(shouldSend({
-            channelMuted: false,
-            threading: unthreaded(),
-            doNotDisturb: false,
-            doNotDisturbOverridden: false,
-            broadcast: false,
-            suppressBroadcast: false,
-            channelNotificationPreference: "everything",
-        }), is, true)
+        expect(shouldSend(tendToSend), is, true)
+    },
+
+    "does not send a notification"() {
+        expect(shouldSend(tendNotToSend), is, false)
     },
 
     "sends a notification when the channel is muted but the user is subscribed to the thread"() {
         expect(shouldSend({
+            ...tendNotToSend,
             channelMuted: true,
             threading: {type: "Threaded", subscribed: true},
             doNotDisturb: false,
-            doNotDisturbOverridden: false,
-            broadcast: false,
-            suppressBroadcast: false,
             channelNotificationPreference: "everything",
         }), is, true)
     },
 
     "does not send a notification when the sender wishes not to be obnoxious"() {
         expect(shouldSend({
-            channelMuted: false,
-            threading: unthreaded(),
+            ...tendToSend,
             doNotDisturb: true,
             doNotDisturbOverridden: false,
-            broadcast: false,
-            suppressBroadcast: false,
-            channelNotificationPreference: "everything",
-        }), is, false)
-    },
-
-    "does not send a notification when the sender wishes not to be obnoxious in a thread"() {
-        expect(shouldSend({
-            channelMuted: true,
-            threading: {type: "Threaded", subscribed: true},
-            doNotDisturb: true,
-            doNotDisturbOverridden: false,
-            broadcast: false,
-            suppressBroadcast: false,
-            channelNotificationPreference: "everything",
         }), is, false)
     },
 
     "sends a notification when the sender wishes to be obnoxious"() {
         expect(shouldSend({
+            ...tendNotToSend,
             channelMuted: false,
-            threading: unthreaded(),
             doNotDisturb: true,
             doNotDisturbOverridden: true,
-            broadcast: false,
-            suppressBroadcast: false,
             channelNotificationPreference: "everything",
         }), is, true)
     },
 
     "sender cannot override a muted channel"() {
         expect(shouldSend({
-            //
+            ...tendToSend,
             channelMuted: true,
-            threading: unthreaded(),
             doNotDisturbOverridden: true,
             doNotDisturb: true,
-            broadcast: false,
-            suppressBroadcast: false,
-            channelNotificationPreference: "everything",
         }), is, false)
     },
 
     "suppressed broadcast"() {
         expect(shouldSend({
-            broadcast: true,
+            ...tendToSend,
             suppressBroadcast: true,
-            channelMuted: false,
-            threading: unthreaded(),
-            doNotDisturbOverridden: false,
-            doNotDisturb: false,
-            channelNotificationPreference: "everything",
         }), is, false)
     },
 
     "broadcast when user wants no notifications for the channel"() {
         expect(shouldSend({
+            ...tendToSend,
             broadcast: true,
             channelNotificationPreference: "nothing",
-            suppressBroadcast: false,
-            channelMuted: false,
-            threading: unthreaded(),
-            doNotDisturbOverridden: false,
-            doNotDisturb: false,
         }), is, false)
-    },
-
-    "non-broadcast when broadcasts are suppressed"() {
-        expect(shouldSend({
-            broadcast: false,
-            suppressBroadcast: true,
-            channelMuted: false,
-            threading: unthreaded(),
-            doNotDisturbOverridden: false,
-            doNotDisturb: false,
-            channelNotificationPreference: "everything",
-        }), is, true)
     },
 })
 
